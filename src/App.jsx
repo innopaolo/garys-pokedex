afddf9ec88b937fa74aff45e60a9dfc9d94c1cc1
Inno Paolo <102833497+innopaolo@users.noreply.github.com>
2023-10-04 11:40:50 +0800

    Add functions for FilterBar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,28 @@ import FilterBar from './FilterBar';
 import dummyPokemonData from './dummyPokemonData';
 
 function App() {
+  const [filteredData, setFilteredData] = useState(dummyPokemonData);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filterType, setFilterType] = useState('');
+
+   const handleSearch = (term) => {
+    setSearchTerm(term);
+    // Perform search logic here and update filteredData accordingly
+    const filteredResults = dummyPokemonData.filter((pokemon) =>
+      pokemon.name.english.toLowerCase().includes(term.toLowerCase())
+    );
+    setFilteredData(filteredResults);
+  };
+
+  // Filtering by pokemon type
+  const handleFilter = (type) => {
+    setFilterType(type);
+    // Perform filtering logic here and update filteredData accordingly
+    const filteredResults = dummyPokemonData.filter((pokemon) =>
+      type === '' || pokemon.type.includes(type)
+    );
+    setFilteredData(filteredResults);
+  };
 
   return (
     <div className="app">
