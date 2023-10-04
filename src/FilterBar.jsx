b96a0802cc6175af7a2fc05309670e94552065b1
Inno Paolo <102833497+innopaolo@users.noreply.github.com>
2023-10-04 12:55:32 +0800

    Add icon to search, filter, and tab

--- src/FilterBar.jsx
+++ src/FilterBar.jsx
@@ -28,29 +28,36 @@ function FilterBar({ onSearch, onFilter }) {
             value={searchTerm}
             onChange={handleSearchChange}
         />
+        <img className="search-icon" src="/search.png" alt="magnifying glass" />
         <button>Search</button>
       </form> 
-      Sort by type <select value={filterType} onChange={handleFilterChange}>
-        <option value="">All Types</option>
-        <option value="normal">Normal</option>
-        <option value="fire">Fire</option>
-        <option value="water">Water</option>
-        <option value="grass">Grass</option>
-        <option value="electric">Electric</option>
-        <option value="ice">Ice</option>
-        <option value="fighting">Fighting</option>
-        <option value="poison">Poison</option>
-        <option value="ground">Ground</option>
-        <option value="flying">Flying</option>
-        <option value="psychic">Psychic</option>
-        <option value="bug">Bug</option>
-        <option value="rock">Rock</option>
-        <option value="ghost">Ghost</option>
-        <option value="dark">Dark</option>
-        <option value="dragon">Dragon</option>
-        <option value="steel">Steel</option>
-        <option value="fairy">Fairy</option>
-      </select>
+      
+      <div className='select-wrapper'>
+      Sort by type&nbsp;&nbsp; 
+            <select value={filterType} onChange={handleFilterChange}>
+                <option value="">All Types</option>
+                <option value="normal">Normal</option>
+                <option value="fire">Fire</option>
+                <option value="water">Water</option>
+                <option value="grass">Grass</option>
+                <option value="electric">Electric</option>
+                <option value="ice">Ice</option>
+                <option value="fighting">Fighting</option>
+                <option value="poison">Poison</option>
+                <option value="ground">Ground</option>
+                <option value="flying">Flying</option>
+                <option value="psychic">Psychic</option>
+                <option value="bug">Bug</option>
+                <option value="rock">Rock</option>
+                <option value="ghost">Ghost</option>
+                <option value="dark">Dark</option>
+                <option value="dragon">Dragon</option>
+                <option value="steel">Steel</option>
+                <option value="fairy">Fairy</option>
+            </select>
+
+            <img className="pokeball-icon" src="/poke-icon.png" alt="pokeball" />
+      </div>
     </div>
   );
 }
