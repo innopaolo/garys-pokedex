4bbab8f4af18c0887d4334d6cb8cef5d8185c3b3
Inno Paolo <102833497+innopaolo@users.noreply.github.com>
2023-10-12 20:39:43 +0800

    Divide App.css into smaller files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import PokemonList from './PokemonList';
-import FilterBar from './FilterBar';
-import PokemonModal from './PokemonModal';
-import PokemonAdd from './PokemonAdd';
-import { fetchData, rebuildPokedexData } from './api-utils.js';
+// Import styles
+import "./styles/App.css";
+import "./styles/CardTypeColors.css";
+import "./styles/FilterBar.css";
+import "./styles/Modals.css";
+import "./styles/PokemonAdd.css";
+import "./styles/PokemonList.css";
+import "./styles/Rebuild.css";
 
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
+import FilterBar from "./FilterBar";
+import PokemonModal from "./PokemonModal";
+import PokemonAdd from "./PokemonAdd";
+import { fetchData, rebuildPokedexData } from "./api-utils.js";
 
 function App() {
   const [displayedPokemonCount, setDisplayedPokemonCount] = useState(15); // Initially display 10 Pokémon cards
@@ -13,18 +20,16 @@ function App() {
 
   const [originalPokemonData, setOriginalPokemonData] = useState([]); // Immutable data to filter from
   const [pokemonData, setPokemonData] = useState([]); // Dynamically changes and will be displayed
-  const [searchTerm, setSearchTerm] = useState('');
-  const [filterType, setFilterType] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filterType, setFilterType] = useState("");
 
   // State for selected Pokemon and modal
-  const [selectedPokemon, setSelectedPokemon] = useState(null); 
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isBlueLightClicked, setIsBlueLightClicked] = useState(false);
   const [isRebuildClicked, setIsRebuildClicked] = useState(false);
 
   const [isNewPokemonAdded, setIsNewPokemonAdded] = useState(false);
-
-
 
   // Function to fetch data from the Express backend and update state
   useEffect(() => {
@@ -35,11 +40,11 @@ function App() {
   // This ensures the new pokemon will show up on search
   // and filter without reloading the website.
   useEffect(() => {
-    fetchData(setPokemonData, setOriginalPokemonData);  
+    fetchData(setPokemonData, setOriginalPokemonData);
   }, [isNewPokemonAdded]);
 
   // Filtering by search term
-   const handleSearch = (term) => {
+  const handleSearch = (term) => {
     setSearchTerm(term);
     // Perform search logic here and update pokemonData accordingly
     const filteredResults = originalPokemonData.filter((pokemon) =>
@@ -53,8 +58,8 @@ function App() {
   const handleFilter = (type) => {
     setFilterType(type);
     // Perform filtering logic on whether all pokemon should show or just the type
-    const filteredResults = originalPokemonData.filter((pokemon) =>
-      type === '' || pokemon.type.includes(type)
+    const filteredResults = originalPokemonData.filter(
+      (pokemon) => type === "" || pokemon.type.includes(type)
     );
     setPokemonData(filteredResults);
     setDisplayedPokemonCount(15); // Reset how many pokemon will show up initially
@@ -65,8 +70,8 @@ function App() {
     setDisplayedPokemonCount((prevCount) => prevCount + increment);
   };
 
-   // Function to open the modal when a card is clicked
-   const handleCardClick = (pokemon) => {
+  // Function to open the modal when a card is clicked
+  const handleCardClick = (pokemon) => {
     setSelectedPokemon(pokemon);
     setIsModalOpen(true);
   };
@@ -82,18 +87,20 @@ function App() {
   // Function to delete pokemon from frontend state
   const handlePokemonDeletion = (pokemonId) => {
     setPokemonData((prevData) => prevData.filter((p) => p.id !== pokemonId));
-    setOriginalPokemonData((prevData) => prevData.filter((p) => p.id !== pokemonId));
+    setOriginalPokemonData((prevData) =>
+      prevData.filter((p) => p.id !== pokemonId)
+    );
   };
 
   // Function to update pokemon's frontend data
   const handlePokemonUpdate = (updatedPokemon) => {
     // Find the index of the updated Pokemon in data
     const index = pokemonData.findIndex((p) => p.id === updatedPokemon.id);
-  
+
     if (index !== -1) {
       const updatedData = [...pokemonData];
       updatedData[index] = updatedPokemon;
-  
+
       // Update the state with the new data
       setPokemonData(updatedData);
     }
@@ -110,7 +117,6 @@ function App() {
     setIsNewPokemonAdded(true);
   };
 
-
   // Function to open the modal for rebuild
   const handleRebuildCLick = () => {
     setIsModalOpen(true);
@@ -121,10 +127,11 @@ function App() {
   const handleRebuild = () => {
     rebuildPokedexData()
       .then(() => {
-          window.location.reload();
-          alert('Rebuild succesful');
-      }).catch((error) => {
-          console.error('Error rebuilding Pokedex:', error);
+        window.location.reload();
+        alert("Rebuild succesful");
+      })
+      .catch((error) => {
+        console.error("Error rebuilding Pokedex:", error);
       });
   };
 
@@ -136,12 +143,16 @@ function App() {
         </div>
       </div>
 
-      <div className='rebuild-btn' onClick={handleRebuildCLick}>Rebuild</div>
+      <div className="rebuild-btn" onClick={handleRebuildCLick}>
+        Rebuild
+      </div>
 
-      <img className='img-header' src="/pokeheader.png" alt="pokedex header" />
+      <img className="img-header" src="/pokeheader.png" alt="pokedex header" />
 
-      <div className='header-descript'>
-        <p>Gary&apos;s <span>super special limited edition</span> pokédex</p>
+      <div className="header-descript">
+        <p>
+          Gary&apos;s <span>super special limited edition</span> pokédex
+        </p>
       </div>
 
       <FilterBar onSearch={handleSearch} onFilter={handleFilter} />
@@ -149,13 +160,16 @@ function App() {
       {pokemonData.length === 0 ? (
         <p>&nbsp;&nbsp;No Pokémon found!</p>
       ) : (
-        <PokemonList pokemonData={pokemonData.slice(0, displayedPokemonCount)} handleCardClick={handleCardClick} />
+        <PokemonList
+          pokemonData={pokemonData.slice(0, displayedPokemonCount)}
+          handleCardClick={handleCardClick}
+        />
       )}
 
       {isModalOpen && selectedPokemon && (
-        <PokemonModal 
-          pokemon={selectedPokemon} 
-          onClose={handleCloseModal} 
+        <PokemonModal
+          pokemon={selectedPokemon}
+          onClose={handleCloseModal}
           onDelete={handlePokemonDeletion}
           onUpdate={handlePokemonUpdate}
         />
@@ -166,10 +180,11 @@ function App() {
       )}
 
       {isModalOpen && isRebuildClicked && (
-        <div className='modal'>
-          <div className='modal-content rebuild-modal'>
+        <div className="modal">
+          <div className="modal-content rebuild-modal">
             <p>
-              You are about to revert the pokédex data to its original form. Proceed?
+              You are about to revert the pokédex data to its original form.
+              Proceed?
             </p>
             <div>
               <button onClick={handleRebuild}>Yesh</button>
@@ -180,7 +195,9 @@ function App() {
       )}
 
       {displayedPokemonCount < pokemonData.length && (
-        <button className='see-more' onClick={handleSeeMoreClick}>See More</button>
+        <button className="see-more" onClick={handleSeeMoreClick}>
+          See More
+        </button>
       )}
     </div>
   );
